53a660f7cf45f851359f762f9e93bf65f6a0dec1
shohag0007 <merinedrive007@gmail.com>
2024-08-21 21:17:42 +0600

    use cutom hooks

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/menu-bg.jpg';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 const Menu = () => {
     return (
         <div>
@@ -10,7 +10,7 @@ const Menu = () => {
 
             </Helmet>
             <Cover img={menuImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
+           
         </div>
     );
 };
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -3,9 +3,9 @@ import { Parallax } from 'react-parallax';
 const Cover = ({img, title}) => {
     return (
         <Parallax
-        blur={{ min: -15, max: 15 }}
+        blur={{ min: -50, max: 50 }}
         bgImage={img}
-        bgImageAlt="the dog"
+        bgImageAlt="the menu"
         strength={-200}
     >
        <div className="hero h-[700px]">
