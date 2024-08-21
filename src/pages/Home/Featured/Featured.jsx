c9d801f881e21ae6dc3f75c842ad7955821541d7
shohag0007 <merinedrive007@gmail.com>
2024-08-21 19:02:40 +0600

    testimonials section is ok

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,13 +5,13 @@ import featuredImg from '../../../assets/home/featured.jpg';
 import './Featured.css';
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle
                 subHeading="check it out"
                 heading="Featured Item"
             ></SectionTitle>
 
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex bg-slate-500 bg-opacity-40 justify-center items-center pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -19,7 +19,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where Can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur expedita delectus beatae incidunt esse similique, accusamus a recusandae, tempora consequuntur modi voluptatum molestiae, suscipit totam? Iusto corporis voluptatum debitis a, recusandae sequi harum deleniti odio temporibus ad consequatur provident, aliquam natus quis cupiditate quidem tempore dolorem distinctio earum laudantium repellat?</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
