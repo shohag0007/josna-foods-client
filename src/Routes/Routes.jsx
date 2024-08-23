2434db7dd9ed0e05135c55740640c3dacb1eed65
shohag0007 <merinedrive007@gmail.com>
2024-08-23 22:58:18 +0600

    firebase authentication process is ready

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,9 @@ import {
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
+import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 
 
@@ -22,6 +25,18 @@ export const router = createBrowserRouter([
         {
           path: 'menu',
           element: <Menu></Menu>
+        },
+        {
+          path: 'order/:category',
+          element: <Order></Order>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
         }
       ]
     },
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,92 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
+
+
+
+
+const SignUp = () => {
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+  const {createUser} = useContext(AuthContext);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password)
+    .then(result =>{
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    })
+  };
+
+  // console.log(watch("example"));
+  return (
+    <>
+      <Helmet>
+        <title>Josna Foods | Sign Up</title>
+
+      </Helmet>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign Up Now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" required />
+                {errors.name && <span className="text-red-600">Name is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" required />
+                {errors.email && <span className="text-red-600">Email is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                })} placeholder="password" className="input input-bordered" required />
+                {errors.password?.type === "required" && (
+                  <p className="text-red-600">password must be characters</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-600">password must be less than 20 characters</p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-600">password must be less than 20 characters</p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-600">password must have one uppercase one lower case, one number and one special characters</p>
+                )}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input className="btn btn-primary" type="submit" value="Sign Up" />
+
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
